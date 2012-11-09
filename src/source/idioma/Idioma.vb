edadf9cb19c1df28e6b4f1578ce240a2f0395075
José Antonio Fuentes Santiago <jafs@jafs.es>
2012-11-09 21:17:55 +0100

    [#39] Resto de ficheros de traducción

---
+++ src/source/idioma/Idioma.vb
@@ -0,0 +1,75 @@
+﻿''' <summary>Clase de obtención de cadenas traducidas.</summary>
+Public Class Idioma
+    ''' <summary>Idioma por defecto</summary>
+    Public Const IDIOMA_DEF As String = "en"
+
+    ''' <summary>Instancia de la clase.</summary>
+    Private Shared objInstancia As Idioma = Nothing
+    ''' <summary>Diccionario que contiene el par clave-valor para el idioma.</summary>
+    Private dicIdioma As New Dictionary(Of String, String)
+
+
+    ''' <summary>Constructor de la clase.</summary>
+    Private Sub New()
+        cargar()
+    End Sub
+
+
+    ''' <summary>Realiza la carga del idioma.</summary>
+    ''' <remarks>En un futuro recibirá el idioma a cargar.</remarks>
+    Private Sub cargar()
+        Dim bCargado As Boolean = False
+        Dim sIdiomas As New List(Of String)
+        sIdiomas.AddRange(My.Resources.sIdiomas.Split(CChar(";")))
+
+        ' Si el idioma existe en la lista de idiomas lo carga
+        If sIdiomas.Contains(My.Application.Culture.TwoLetterISOLanguageName.ToLower) Then
+            Select Case My.Application.Culture.TwoLetterISOLanguageName.ToLower
+                Case "es"
+                    XmlParser.cargar(dicIdioma, My.Resources.es)
+                    bCargado = True
+            End Select
+        End If
+
+        ' En caso de no cargase el idioma, carga por defecto Inglés.
+        If Not bCargado Then
+            XmlParser.cargar(dicIdioma, My.Resources.en)
+        End If
+    End Sub
+
+
+    ''' <summary>Obtiene la instancia de la clase.</summary>
+    ''' <returns>Instancia de la clase.</returns>
+    Private Shared Function getInstancia() As Idioma
+        If objInstancia Is Nothing Then
+            objInstancia = New Idioma
+        End If
+
+        Return objInstancia
+    End Function
+
+
+    ''' <summary>Obtiene la traducción de la clave recibida como parámetro.</summary>
+    ''' <param name="sClave">Clave a traducir.</param>
+    ''' <returns>Cadena con la traducción.</returns>
+    Public Shared Function traducir(ByRef sClave As String) As String
+        Return getInstancia.getCadena(sClave)
+    End Function
+
+
+    ''' <summary>Obtiene la cadena de traducción correspondiente a la clave recibida.</summary>
+    ''' <param name="sClave">Clave a traducir.</param>
+    ''' <returns>Cadena con la traducción.</returns>
+    Private Function getCadena(ByRef sClave As String) As String
+        Dim sValor As String
+
+        If dicIdioma.ContainsKey(sClave) Then
+            sValor = dicIdioma.Item(sClave)
+        Else
+            sValor = sClave
+        End If
+
+        Return sValor
+    End Function
+End Class
+
---
+++ src/source/idioma/XmlParser.vb
@@ -0,0 +1,60 @@
+﻿Imports System.Xml
+Imports System.IO
+
+
+''' <summary>Extrae las cadenas de traducción de un fichero XML.</summary>
+Public Class XmlParser
+    ''' <summary>Indica un elemento de idioma</summary>
+    Private Const ELEMENTO As String = "tag"
+    ''' <summary>Nombre del atributo clave del elemento.</summary>
+    Private Const AT_CLAVE As String = "clave"
+    ''' <summary>Nombre del atributo valor del elemento.</summary>
+    Private Const AT_VALOR As String = "valor"
+
+
+    ''' <summary>Carga el diccionario de idioma actual.</summary>
+    ''' <param name="dicIdioma">Diccionario de idioma a cargar</param>
+    ''' <param name="sValores">Cadena con los datos</param>
+    Public Shared Sub cargar(ByRef dicIdioma As Dictionary(Of String, String), ByRef sValores As String)
+        ' Lector de flujo para cadenas
+        Dim objFlujo As New StringReader(sValores)
+        ' Lector de XML
+        Dim objLector As XmlTextReader = New XmlTextReader(objFlujo)
+        ' Elemento actual
+        Dim sActual As String
+        ' Clave actual
+        Dim sClave As String
+        ' Valor actual
+        Dim sValor As String
+
+        ' Inicializa el diccionario
+        dicIdioma.Clear()
+
+        Do While (objLector.Read())
+            Try
+                ' Analiza el elemento actual.
+                If objLector.NodeType = XmlNodeType.Element Then
+                    sActual = objLector.Name
+                    sClave = String.Empty
+                    sValor = String.Empty
+
+                    ' Si hay atributos se mira si está en un elemento tag.
+                    If objLector.HasAttributes And sActual = ELEMENTO Then
+                        While objLector.MoveToNextAttribute()
+                            If objLector.Name = "clave" Then
+                                sClave = objLector.Value
+                            ElseIf objLector.Name = "valor" Then
+                                sValor = objLector.Value
+                            End If
+
+                            If Not dicIdioma.ContainsKey(sClave) And sValor.Length > 0 Then
+                                dicIdioma.Add(sClave, sValor)
+                            End If
+                        End While
+                    End If
+                End If
+            Catch e As Exception
+            End Try
+        Loop
+    End Sub
+End Class
